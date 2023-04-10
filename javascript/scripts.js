e68a3d76ea35a5e3873d44ee219b6744af7a7cfa
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-04-10 21:07:07 +0800

    block everything

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -19,6 +19,6 @@ const toggleHamburger = () => {
     var hamburgerMenu = document.getElementById("hamburgerMenu")
     hamburgerMenu.classList.toggle('active')
 
-    var everythingBlocker = document.getElementByClass("blockEverything")
+    var everythingBlocker = document.getElementById("blockEverything")
     everythingBlocker.classList.toggle('active')
 }
