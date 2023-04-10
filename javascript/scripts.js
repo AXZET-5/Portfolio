edefe844e21c82f36eb63afccb62e1849e4d24d8
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-04-10 21:12:36 +0800

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,19 +6,12 @@ const snapToTop = () => {
     })
 }
 
-/*const toggleHamburger = () => {
-    var hamburgerMenu = document.getElementById("hamburgerMenu")
-    if (hamburgerMenu.style.display === "inline"){
-        hamburgerMenu.style.display = "none"
-    } else {
-        hamburgerMenu.style.display = "inline"
-    }
-}*/
-
 const toggleHamburger = () => {
     var hamburgerMenu = document.getElementById("hamburgerMenu")
+    console.log(hamburgerMenu)
     hamburgerMenu.classList.toggle('active')
 
     var everythingBlocker = document.getElementById("blockEverything")
+    console.log(everythingBlocker)
     everythingBlocker.classList.toggle('active')
 }
