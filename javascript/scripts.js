3946bbc5857937f174e8185278b96ba0325fc89f
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-04-12 16:04:02 +0800

    reveal testing

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -21,3 +21,21 @@ const closeMenu = () => {
     var everythingBlocker = document.getElementById("blockEverything")
     everythingBlocker.classList.remove('active')
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".Reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
