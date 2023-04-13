d33940f19e79b4ac082935aa5e1827542c4172c1
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-04-13 18:14:45 +0800

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -21,10 +21,10 @@ const changeHamburgerIcon = () => {
   var hamburgerMenu = document.getElementById("hamburgerMenu")
     var hamburgerIcon = document.getElementById("hamburgerIcon")
     if (hamburgerMenu.classList.contains('active')) {
-      hamburgerIcon.innerHTML = '<img src="./images/Close.svg" class="HamburgerInnerIcon">'
+      hamburgerIcon.innerHTML = '☰'
     }
     else {
-      hamburgerIcon.innerHTML = '<img src="./images/Menu.svg" class="HamburgerInnerIcon">'
+      hamburgerIcon.innerHTML = '⨠'
     }
 }
 
